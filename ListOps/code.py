563c6e1583763dd6fa678f9bd2f65b3e1a68cd4b
hemankithreddy <hemankith@gmail.com>
2019-10-20 03:57:07 +0530

    lol

--- ListOps/code.py
+++ ListOps/code.py
@@ -1,18 +1,8 @@
-'''
-Task: Delete alternate elements of the given list, starting from index 0,
-      print the list after each traversal.
-
-Example: given list -> [1,2,3,4,5]
-                o/p: [2, 4]
-                     [4]
-                     []
-
-'''
-
 nums = [3, 5, 2, 9, 101, 333, 4]
-
-while len(nums):
-    for i in range(len(nums)):
-        if i%2==0:
-            del nums[i]
-    print(nums)
+i=0
+def remove_odd_element(a):
+    while i in range(len(num)+1):
+      del nums[0::2]
+      print(nums)
+      i=i+1
+print("[]")

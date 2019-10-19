a28d428542d58f286dc5ff9c5ffba54b7f63265a
hemankithreddy <hemankith@gmail.com>
2019-10-20 04:03:08 +0530

    L

--- ListOps/code.py
+++ ListOps/code.py
@@ -1,8 +1,9 @@
 nums = [3, 5, 2, 9, 101, 333, 4]
 i=0
 def remove_odd_element(a):
-    while i in range(len(num)+1):
-      del nums[0::2]
-      print(nums)
+    while i in range(len(a)+1):
+      del a[0::2]
+      print(a)
       i=i+1
 print("[]")
+remove_odd_element(nums)

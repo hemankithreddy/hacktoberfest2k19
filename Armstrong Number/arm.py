34a9e70e2742d8dfd55764db5e296e50889119b9
hemankithreddy <hemankith@gmail.com>
2019-10-20 00:23:56 +0530

    Armstrong

---
+++ Armstrong Number/arm.py
@@ -0,0 +1,17 @@
+def arm(n):
+	l=str(n)
+	leng=len(l)
+	for i in range(leng):
+		cube=cube+l[i]**3
+	return cube
+while True:
+	x=int(input("enter a number:"))
+	if(x>0):
+		y=arm(n)
+		break
+	else:
+		print('Wrong input')
+if(x==y):
+	print('The entered number is an armstrong number")
+else:
+	print('The entered number is not an armstrong number")
